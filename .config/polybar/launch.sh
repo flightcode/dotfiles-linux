0221a259d3c4ad313dcc019dafe5b35b1a17e20d
flightcode <flightcode@pm.me>
2021-10-07 08:52:29 +1100

    Add multiple monitor support

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,3 +6,7 @@ killall -q polybar
 # Launch Bars
 echo "---" | tee -a /tmp/polybar1.log
 polybar topbar 2>&1 | tee -a /tmp/polybar1.log & disown
+
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+	MONITOR=$m polybar --reload topbar &
+done
