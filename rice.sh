870ff403596877ac3761a6a54371d4ef6420eff9
flightcode <flightcode@pm.me>
2022-06-13 18:05:21 +1000

    Add basic install script

---
+++ rice.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Install font
+yay -S --noconfirm nerd-fonts-source-code-pro
+
+#Install zsh & starship
+yay -S --noconfirm zsh starship
+chsh -s $(which zsh)
+
+#Install essential programs
+yay -S --noconfirm picom bspwm sxhkd betterlockscreen feh polybar rofi kitty ranger pywal neofetch imagemagick redshift
+
+# #Configs
+# chmod +x ~/.config/polybar/launch.sh
+# gsettings set org.gnome.desktop.interface font-name "NotoSans Nerd Font"
+# gsettings set org.gnome.desktop.interface document-font-name "NotoSans Nerd Font"
+# gsettings set org.gnome.desktop.interface monospace-font-name "NotoSans Nerd Font Mono"
